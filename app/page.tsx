eb19ce897e63a6119cb0154590f0f32494e431b2
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-10-06 21:04:58 +0100

    Searchbar, Carousel initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import Searchbar from '@/components/Searchbar'
+import HeroCarousel from '@/components/HeroCarousel'
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24 border-2 ">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -24,11 +26,13 @@ const Home = () => {
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
-            Searchbar
+
+            <Searchbar />
           </div>
-          HeroCarousel
         </div>
+        <HeroCarousel />
       </section>
+
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
