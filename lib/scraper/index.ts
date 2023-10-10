fb61b2653b461bf5f3c4c6317b3b4e9fe3d3b93f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-10-10 20:53:15 +0100

    Extracting currentPrice, originalPrice based on regex

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
+import { extractPrice } from '../utils'
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return
 
@@ -25,8 +26,21 @@ export async function scrapeAmazonProduct(url: string) {
 
     // Extract the product title
     const title = $('#productTitle').text().trim()
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base')
+    )
 
-    console.log({ title })
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('$listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    )
+
+    console.log({ title, currentPrice, originalPrice })
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`)
   }
